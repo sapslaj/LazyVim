41cc955021da8be7f5da1352d2ee570decab226b
sapslaj <saps.laj@gmail.com>
2023-10-11 16:55:26 -0400

    stop splitting terminal window

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -50,7 +50,7 @@ return {
     end,
     opts = {
       sources = { "filesystem", "buffers", "git_status", "document_symbols" },
-      open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
+      open_files_do_not_replace_types = { "Trouble", "qf", "Outline" },
       filesystem = {
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
